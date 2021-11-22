87b702800e95d9443b19d7a51bb387c21d572bbb
William Wold <wm@wmww.sh>
2021-11-22 16:50:37 -0500

    Fix typechecking

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .core.mission import mission
-from .core.result import SuccessResult, UndoneResult, FailedResult
-from .core.task import task
-from .core.context import info, Info, RunCtx
--- instigate.py
+++ instigate.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from . import core
-else:
-    import core
+from core.mission import mission
+from core.result import SuccessResult, UndoneResult, FailedResult
+from core.task import task
+from core.context import info, Info, RunCtx
 
 if __name__ == '__main__':
+    import core
     core.runner.main()
-
